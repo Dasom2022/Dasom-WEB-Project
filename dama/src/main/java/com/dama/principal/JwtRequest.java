9c80541e0998bf671fc271fa4c03b8ec75db53e0
Lee.t.c <seonghoo1217@naver.com>
2022-04-14 21:06:35 +0900

    feat: jwt userDetails

---
+++ dama/src/main/java/com/dama/principal/JwtRequest.java
@@ -0,0 +1,25 @@
+package com.dama.principal;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class JwtRequest implements Serializable {
+
+    private static final long serialVersionUID = 5926468583005150707L;
+
+    private String username;
+    private String password;
+
+    //need default constructor for JSON Parsing
+    public JwtRequest() {   }
+
+    public JwtRequest(String username, String password) {
+        this.username=username;
+        this.password=password;
+    }
+
+}
---
+++ dama/src/main/java/com/dama/service/JwtUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.dama.service;
+
+import com.dama.model.entity.Member;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if ("user_id".equals(username)) {
+            /*return new Member("user_id", "$2a$10$m/enYHaLsCwH2dKMUAtQp.ksGOA6lq7Fd2pnMb4L.yT4GyeAPRPyS",
+                    new ArrayList<>());*/
+            return null;
+        } else {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+    }
+}
