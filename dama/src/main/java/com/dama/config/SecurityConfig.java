402f559e77f38bc9f7653f079a09886d0b1d1cfd
Lee.t.c <seonghoo1217@naver.com>
2022-05-17 15:07:51 +0900

    fix:cors

--- dama/src/main/java/com/dama/config/SecurityConfig.java
+++ dama/src/main/java/com/dama/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -27,23 +28,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-
-       /* http.logout()
-                .logoutUrl("/doLogout");*/
+        http
+                .formLogin().disable()
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.authorizeRequests()
                 .antMatchers("/")
                 .permitAll()
                 .anyRequest()
-                .permitAll()
-                .and()
-                .formLogin()
-                .loginPage("/member/signin")
-                .loginProcessingUrl("/member/signin")
-                .defaultSuccessUrl("/")
-                .usernameParameter("username")
-                .passwordParameter("password");
+                .permitAll();
     }
 
     /*@Override
