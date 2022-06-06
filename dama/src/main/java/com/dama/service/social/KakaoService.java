d77d404283bd1d611dd3b906124b79a970d5217a
Lee.t.c <seonghoo1217@naver.com>
2022-06-06 13:24:32 +0900

    fix: kakao social Login

--- dama/src/main/java/com/dama/service/social/KakaoService.java
+++ dama/src/main/java/com/dama/service/social/KakaoService.java
@@ -61,12 +61,16 @@ public class KakaoService {
             System.out.println("kakao_account = " + kakao_account);
             String nickname = properties.getAsJsonObject().get("nickname").getAsString();
             String imgURL = properties.getAsJsonObject().get("profile_image").getAsString();
+/*
             String email = kakao_account.getAsJsonObject().get("email").getAsString();
+*/
 
             //    UserRequestDto에 값 주입
             userInfo.setUsername(nickname);
             userInfo.setSocialId(kakaoId);
+/*
             userInfo.setEmail(email);
+*/
             userInfo.setImgURL(imgURL);
             userInfo.setSocialType(SocialType.KAKAO);
 
