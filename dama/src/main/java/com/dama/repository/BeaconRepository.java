0f24ae1eeca93aace5767a2520497b5797f893d3
Lee.t.c <seonghoo1217@naver.com>
2022-06-07 17:11:29 +0900

    fix: sensor api db refactoring

--- dama/src/main/java/com/dama/repository/BeaconRepository.java
+++ dama/src/main/java/com/dama/repository/BeaconRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface BeaconRepository extends JpaRepository<Beacon,Long> {
     Optional<Beacon> findByObname(String obname);
+
 }
--- dama/src/main/java/com/dama/service/BeaconService.java
+++ dama/src/main/java/com/dama/service/BeaconService.java
@@ -6,15 +6,18 @@ import com.dama.repository.BeaconRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Log4j2
+@Transactional(readOnly = true)
 public class BeaconService {
     private final BeaconRepository beaconRepository;
 
+    @Transactional
     public void insertionOrUpdate(BeaconDto beaconDto){
         System.out.println("beaconDto.getOb_name() = " + beaconDto.getOb_name());
         System.out.println("beaconDto = " + beaconDto.getBeacon());
