817dbb34dc5fc833a10c5b8bf6cce45eac3c1edd
Lee.t.c <seonghoo1217@naver.com>
2022-06-28 15:37:02 +0900

    feat: return Api memberDelete

--- dama/src/main/java/com/dama/controller/api/ApiMemberController.java
+++ dama/src/main/java/com/dama/controller/api/ApiMemberController.java
@@ -4,6 +4,7 @@ import com.dama.model.dto.response.ApiMemberStateResponseDto;
 import com.dama.model.entity.Member;
 import com.dama.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/member/auth")
 @RequiredArgsConstructor
+@Log4j2
 public class ApiMemberController {
 
     private final MemberService memberService;
@@ -25,6 +27,7 @@ public class ApiMemberController {
 
     @DeleteMapping("/delete")
     public ResponseEntity<String> returnApiMemberDelete(@RequestParam("username") String username){
+        log.info("deleteMapping 성공");
         memberService.memberDelete(username);
         return new ResponseEntity<>("회원삭제 API 성공!",HttpStatus.OK);
     }
