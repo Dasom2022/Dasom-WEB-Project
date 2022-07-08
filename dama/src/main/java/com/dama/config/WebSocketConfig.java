e7863e73f66c48472a9864a73abfc4f4dbd4d091
Lee.t.c <seonghoo1217@naver.com>
2022-07-08 17:33:56 +0900

    fix: web socket subscribe url

--- dama/src/main/java/com/dama/config/WebSocketConfig.java
+++ dama/src/main/java/com/dama/config/WebSocketConfig.java
@@ -16,10 +16,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-/*
-    private final TopicSubscriptionInterceptor topicSubscriptionInterceptor;
-*/
-
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/sub");
@@ -28,11 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stomp/chat").setAllowedOrigins("http://43.200.61.12:3333","http://localhost:3000").withSockJS();
+        registry.addEndpoint("/stomp").setAllowedOrigins("http://localhost:3000").withSockJS();
     }
 
-    /*@Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.setInterceptors(topicSubscriptionInterceptor);
-    }*/
 }
--- dama/src/main/java/com/dama/controller/api/StompController.java
+++ dama/src/main/java/com/dama/controller/api/StompController.java
@@ -7,13 +7,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @Log4j2
 public class StompController {
@@ -21,8 +23,8 @@ public class StompController {
     private final SimpMessagingTemplate template;
 
     @MessageMapping("/api/websocket/itemList")
-    public void enter(ItemListResponseDto itemListResponseDto) {
+    public void enter(@Payload ItemListResponseDto itemListResponseDto) {
         System.out.println("itemListResponseDto = " + itemListResponseDto.getItemName());
-        template.convertAndSend("http://locathost:3000/sub/chat/read/",itemListResponseDto.getItemName());
+        template.convertAndSend("/sub/chat/read/",itemListResponseDto.getItemName());
     }
 }
