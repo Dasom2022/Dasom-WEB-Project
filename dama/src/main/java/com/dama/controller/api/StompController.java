ccf339bd35b3983bb2057448eb0f7f3846e99dba
Lee.t.c <seonghoo1217@naver.com>
2022-07-12 14:46:28 +0900

    fix: Object Recognition Api Url

--- dama/src/main/java/com/dama/controller/api/StompController.java
+++ dama/src/main/java/com/dama/controller/api/StompController.java
@@ -1,5 +1,6 @@
 package com.dama.controller.api;
 
+import com.dama.model.dto.BeaconDto;
 import com.dama.model.dto.response.ItemListResponseDto;
 import com.dama.model.entity.Member;
 import com.dama.repository.MemberRepository;
@@ -10,6 +11,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -23,8 +25,11 @@ public class StompController {
     private final SimpMessagingTemplate template;
 
     @MessageMapping("/api/websocket/itemList")
-    public void enter(@Payload ItemListResponseDto itemListResponseDto) {
-        System.out.println("itemListResponseDto = " + itemListResponseDto.getItemName());
-        template.convertAndSend("/sub/chat/read/",itemListResponseDto);
+    public void enter(@RequestParam("ob_name") String ob_name,@RequestParam("beacon")String beacon) {
+        System.out.println("ob_name = " + ob_name);
+        BeaconDto dto=new BeaconDto();
+        dto.setBeacon(beacon);
+        dto.setOb_name(ob_name);
+        template.convertAndSend("/sub/chat/read/",dto);
     }
 }
